5f706425e753897666215bddf3beab0b1ec87ef9
Anmin Gong <agong@vmware.com>
2012-12-04 09:16:10 +0800

    add sliding drawer demo activity

---
+++ src/net/cmoaciopm/demo/view/SlidingDrawerActivity.java
@@ -0,0 +1,20 @@
+package net.cmoaciopm.demo.view;
+
+import net.cmoaciopm.demo.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.SlidingDrawer;
+
+public class SlidingDrawerActivity extends Activity {
+	
+	private SlidingDrawer mSlidingDrawer;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sliding_drawer);
+		
+		mSlidingDrawer = (SlidingDrawer)findViewById(R.id.sliding_drawer);
+	}
+
+}
